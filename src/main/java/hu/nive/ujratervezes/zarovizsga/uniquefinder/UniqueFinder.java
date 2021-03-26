74d4605a1dc82d6fb558a050a069561559fca627
obqo <ati.sz@freemail.hu>
2021-03-26 19:33:16 +0100

    uniquefinder-end

--- src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
@@ -1,4 +1,20 @@
 package hu.nive.ujratervezes.zarovizsga.uniquefinder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UniqueFinder {
+
+    public List<Character> uniqueChars(String str) {
+        if (str==null) {
+            throw new IllegalArgumentException("String is empty!");
+        }
+        List<Character> result = new ArrayList<>();
+        for (char item : str.toCharArray()) {
+            if (!result.contains(item)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
 }
